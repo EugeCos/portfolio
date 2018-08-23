825f3a08a78f90a987b648a13651c0a623107d0d
unknown <madskillz@gmx.com>
2018-08-23 18:23:23 -0400

    added functional portfolio page elements

--- src/components/View-Container/Portfolio/Portfolio.jsx
+++ src/components/View-Container/Portfolio/Portfolio.jsx
@@ -5,19 +5,19 @@ export default class Portfolio extends Component {
   constructor() {
     super();
     this.state = {
+      hover: false,
+      hoveredProject: "",
       projects: [
         {
           name: "Velvet Crypto",
-          description:
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, dolore.",
+          description: "Cryptocurrency exchange app",
           imageHover: "/img/velvet.jpg",
           imageBackground: "/img/velvet_bw.jpg",
           index: 1
         },
         {
           name: "Fairmont Property Management",
-          description:
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, dolore.",
+          description: "Property management site with custom CMS platform",
           imageHover: "/img/fairmont.jpg",
           imageBackground: "/img/fairmont_bw.jpg",
           index: 2
@@ -25,16 +25,84 @@ export default class Portfolio extends Component {
       ]
     };
   }
+
+  hoverOn = projectName => {
+    this.setState({
+      hover: true,
+      hoveredProject: projectName
+    });
+  };
+
+  hoverOff = () => {
+    this.setState({
+      hover: false,
+      hoveredProject: ""
+    });
+  };
+
   render() {
-    const { projects } = this.state;
+    const { projects, hoveredProject, hover } = this.state;
     let projectJSX = projects.map(project => {
       return (
-        <div className="project-container" key={project.index}>
+        <div
+          className="project-container"
+          key={project.index}
+          onMouseEnter={() => this.hoverOn(project.name)}
+          onMouseLeave={() => this.hoverOff(project.name)}
+        >
+          <div
+            className={`${
+              hoveredProject === project.name
+                ? "project-number-hover"
+                : "project-number"
+            }`}
+          >
+            {project.index}
+          </div>
           <img
             className="project-image"
-            src={project.imageBackground}
+            src={
+              hover
+                ? hoveredProject === project.name
+                  ? project.imageHover
+                  : project.imageBackground
+                : project.imageBackground
+            }
             alt="image"
           />
+          <div
+            className={`${
+              hoveredProject === project.name
+                ? "hover-style hover-overlay"
+                : "dark-overlay"
+            }`}
+          />
+          <div
+            className={`${
+              hoveredProject === project.name
+                ? "project-name-container-hover"
+                : "project-name-container"
+            }`}
+          >
+            <h3
+              className={`${
+                hoveredProject === project.name
+                  ? "project-name-hover"
+                  : "project-name"
+              }`}
+            >
+              {project.name}
+            </h3>
+            <h4
+              className={`${
+                hoveredProject === project.name
+                  ? "project-description-hover"
+                  : "project-description"
+              }`}
+            >
+              {project.description}
+            </h4>
+          </div>
         </div>
       );
     });
